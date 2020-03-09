47f2db6d9c0fea0d1233c84a2d7241ea33cd088d
shanghuifu <shanghuifu@gmail.com>
2020-03-09 21:18:24 +0900

    予約認識メソッドの実装

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -5,4 +5,15 @@ class Reservation < ApplicationRecord
 
   belongs_to :patient
   belongs_to :schedule
+
+  require "date"
+  
+  def rsv_search(schedule_id, days)
+    none = ""
+    if self.date == Date.today + days && self.schedule_id == schedule_id
+      return self.patient.name
+    else
+      return none 
+    end
+  end
 end
