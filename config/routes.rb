267bb97d1b457b8968c08200a1e607abca30e29e
shanghuifu <shanghuifu@gmail.com>
2020-03-24 21:10:09 +0900

    add routing

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   resources :patients, only: [:index, :new, :create, :show] do
     resources :logs, only: :create
   end
-  resources :reservations, only: [:index, :new, :create]
+  resources :reservations, except: :show
   root to: "reservations#index"
 end
