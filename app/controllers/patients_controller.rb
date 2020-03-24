142fe403f8d8f4a0fcd0fa07f5f3ac5ee72608c3
shanghuifu <shanghuifu@gmail.com>
2020-03-24 21:53:00 +0900

    add actions(edit, update, destroy)

--- app/controllers/patients_controller.rb
+++ app/controllers/patients_controller.rb
@@ -22,6 +22,23 @@ class PatientsController < ApplicationController
     @log     = Log.new
     @rsvs    = Reservation.where(patient_id: params[:id])
                           .where('date >= ?', Date.today)
+                          .order(:date)
+  end
+
+  def edit
+    @patient = Patient.find(params[:id])
+  end
+
+  def update
+    patient = Patient.find(params[:id])
+    patient.update(patient_params)
+    redirect_to patient_path(params[:id])
+  end
+
+  def destroy
+    patient = Patient.find(params[:id])
+    patient.destroy
+    redirect_to reservations_path
   end
 
   private
