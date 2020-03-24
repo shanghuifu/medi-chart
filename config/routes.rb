28439d2ebb949d6e288141c5ad1a44be5c677359
shanghuifu <shanghuifu@gmail.com>
2020-03-24 21:52:22 +0900

    add routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  resources :patients, only: [:index, :new, :create, :show] do
+  resources :patients do
     resources :logs, only: :create
   end
   resources :reservations, except: :show
